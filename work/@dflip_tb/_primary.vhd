5d4e2d44436698ddaafa19f9ba295c878a32acbf
Muhammad Somaan <m.somaan@gmail.com>
2025-11-02 09:10:03 +0500

    Delete work directory

--- work/@dflip_tb/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity Dflip_tb is
-end Dflip_tb;
--- work/mux4to1_tb/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity mux4to1_tb is
-    generic(
-        size            : integer := 4
-    );
-    attribute mti_svvh_generic_type : integer;
-    attribute mti_svvh_generic_type of size : constant is 1;
-end mux4to1_tb;
