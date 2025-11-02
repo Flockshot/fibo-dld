6ff14687fe9362a3cf038f2480f86444cc7d102c
Muhammad Somaan <m.somaan@gmail.com>
2025-11-02 09:09:34 +0500

    Delete Datapath directory

--- Datapath/work/@fibo_datapath_tb1/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity Fibo_datapath_tb1 is
-    generic(
-        size            : integer := 4
-    );
-    attribute mti_svvh_generic_type : integer;
-    attribute mti_svvh_generic_type of size : constant is 1;
-end Fibo_datapath_tb1;
